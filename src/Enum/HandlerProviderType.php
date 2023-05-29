62c59670aa9a71ddd936b7f40651285617376c6a
designcise <dev@designcise.com>
2023-05-29 18:28:15 +0200

    refactor: added todo

--- src/Enum/HandlerProviderType.php
+++ src/Enum/HandlerProviderType.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace BitFrame\Whoops\Enum;
 
+// @TODO when using enums as array keys is allowed, this should be refactored to use `case` syntax
 enum HandlerProviderType: string
 {
-    /** @var string */
     final public const HTML = 'html';
     final public const JSON = 'json';
     final public const JSONP = 'jsonp';
